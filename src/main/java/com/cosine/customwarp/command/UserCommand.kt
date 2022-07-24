17fe5f1ab715bdab8c21c2630217ca41d65e117b
CosineAA <rhdwlgns0725@gmail.com>
2022-07-25 00:26:49 +0900

    update

--- src/main/java/com/cosine/customwarp/command/UserCommand.kt
+++ src/main/java/com/cosine/customwarp/command/UserCommand.kt
@@ -2,6 +2,7 @@ package com.cosine.customwarp.command
 
 import com.cosine.customwarp.util.Bag
 import kr.msleague.util.extensions.addNBTTagCompound
+import kr.msleague.util.extensions.checkFunc
 import kr.msleague.util.extensions.percent
 import kr.msleague.util.locale.l10nDisplayName
 import org.bukkit.command.Command
@@ -14,9 +15,22 @@ class UserCommand : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (sender is Player) {
             val player: Player = sender
-            player.inventory.itemInMainHand.apply {
-                itemMeta = addNBTTagCompound(Bag(27)).itemMeta
+            checkFunc {
+                player.inventory.itemInMainHand.apply {
+                    itemMeta = addNBTTagCompound(Bag(27)).itemMeta
+                }
+            }.apply { // 앞에 Object
+                player.sendMessage(toString())
             }
+//            checkFunc {
+//                player.inventory.itemInMainHand.apply {
+//                    itemMeta = addNBTTagCompound(Bag(27)).itemMeta
+//                }
+//            }.also { test -> // 앞에 Object
+//                test.toString()
+//                this // 클래스
+//            }
+//          apply -> this, also -> this를 쓰거나 람다로 이름 지정
             return false
         }
         return false
